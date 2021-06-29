4b0a65f637b24dd4bb7736c9c0fd96e03eb307da
Ben Gamari <ben@smart-cactus.org>
2021-06-29 09:50:04 -0400

    Factor out helper bindings

--- Bench.hs
+++ Bench.hs
@@ -1,10 +1,13 @@
 {-# LANGUAGE BangPatterns #-}
-module Main where
+module Main (main, l, lInline) where
 import qualified Encoding as A
 import BenchLib
 
 
+l = A.encodingToLazyByteString . A.list A.int
+lInline = A.encodingToLazyByteString . A.inlineList A.int
+
 main = do
-  bench "L" $ nf (A.encodingToLazyByteString . A.list A.int) [1 :: Int ..1000]
-  bench "L-inline" $ nf (A.encodingToLazyByteString . A.inlineList A.int) [1 :: Int ..1000]
+  bench "L" $ nf l [1 :: Int ..1000]
+  bench "L-inline" $ nf lInline [1 :: Int ..1000]
 
